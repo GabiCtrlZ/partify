af8ecf66cb844e3ba1505e3e22eed799194bc11d
unknown <chen572@gmail.com>
2020-12-21 00:15:50 +0200

    added share to whatsapp button

--- packages/client/src/Components/Header.jsx
+++ packages/client/src/Components/Header.jsx
@@ -5,7 +5,7 @@ import Button from '@material-ui/core/Button'
 import copy from 'copy-to-clipboard'
 import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
-import FileCopyIcon from '@material-ui/icons/FileCopy'
+import { FileCopy, WhatsApp } from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
 import { connect } from 'react-redux'
 import Typography from '@material-ui/core/Typography'
@@ -13,79 +13,91 @@ import { get } from 'lodash'
 import { playSnackbar } from '../lib/snackbar'
 // import Logo from '../assets/Icons/Logo'
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    height: theme.measurements.headerHeight,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: theme.spacing(2),
-    background: 'black',
-    zIndex: 3,
-    border: 0,
-  },
-  menuItem: {
-    color: 'white',
-  },
-  accountIcon: {
-    color: 'white',
-    borderColor: fade('#FFF', 0.5),
-    padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`,
-    borderRadius: 8,
-    textTransform: 'none',
-  },
-  menu: {
-    background: 'rgba(0, 0, 0, 1)',
-  },
-  logoContainer: {
-    display: 'flex',
-    color: 'white',
-    fontWeight: 'bold',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    userSelect: 'none',
-    '& > div': {
-      // marginTop: 4,
-      // borderTop: '1px solid',
-      letterSpacing: '1.5px',
-      fontSize: 20,
+const useStyles = makeStyles(
+  (theme) => ({
+    appBar: {
+      height: theme.measurements.headerHeight,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: theme.spacing(2),
+      background: 'black',
+      zIndex: 3,
+      border: 0,
     },
-  },
-}), { name: 'Header' })
+    menuItem: {
+      color: 'white',
+    },
+    accountIcon: {
+      color: 'white',
+      borderColor: fade('#FFF', 0.5),
+      padding: `${theme.spacing(0.5)}px ${theme.spacing(2)}px`,
+      borderRadius: 8,
+      textTransform: 'none',
+    },
+    menu: {
+      background: 'rgba(0, 0, 0, 1)',
+    },
+    logoContainer: {
+      display: 'flex',
+      color: 'white',
+      fontWeight: 'bold',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-start',
+      userSelect: 'none',
+      '& > div': {
+        // marginTop: 4,
+        // borderTop: '1px solid',
+        letterSpacing: '1.5px',
+        fontSize: 20,
+      },
+    },
+  }),
+  { name: 'Header' }
+)
 
 function Header(props) {
   const classes = useStyles(props)
-  const {
-    name,
-    room,
-  } = props
+  const { name, room } = props
   const [anchorEl, setAnchorEl] = useState(null)
 
   const handleClose = () => setAnchorEl(null)
 
   const copyRoomLink = () => {
-    copy(`${get(window.location.href.split('?'), '0', 'defultRoute')}?roomId=${room}`)
-    playSnackbar('Copied room link to clipboard!', { variant: 'success' })
+    if (room) {
+      copy(
+        `${get(
+          window.location.href.split('?'),
+          '0',
+          'defultRoute'
+        )}?roomId=${room}`
+      )
+      return playSnackbar('Copied room link to clipboard!', {
+        variant: 'success',
+      })
+    }
+
+    playSnackbar('First you need to create rtoom blyaat', { variant: 'error' })
   }
 
   return (
     <AppBar
-      color="primary"
-      variant="outlined"
-      position="static"
+      color='primary'
+      variant='outlined'
+      position='static'
       classes={{ root: classes.appBar }}
     >
       <div className={classes.logoContainer}>
-        {/* <Logo /> */}
         <div>Partify</div>
+        {/* <Logo /> */}
       </div>
       {name && (
         <>
           <Button
             classes={{ root: classes.accountIcon }}
             onClick={({ currentTarget }) => setAnchorEl(currentTarget)}
-            variant="outlined"
+            variant='outlined'
           >
             {name}
           </Button>
@@ -94,33 +106,42 @@ function Header(props) {
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
-            <div
-              className={classes.menu}
-            >
-              <MenuItem
-                className={classes.menuItem}
-                onClick={copyRoomLink}
-              >
+            <div className={classes.menu}>
+              <MenuItem className={classes.menuItem} onClick={copyRoomLink}>
                 <div>
                   <div style={{ fontSize: 11 }}>Room Code</div>
-                  <Typography color="secondary">{room || 'No room'}</Typography>
+                  <Typography color='secondary'>{room || 'No room'}</Typography>
                 </div>
                 {room && (
-                  <IconButton
-                    className={classes.menuItem}
-                    aria-label="copy"
-                  >
-                    <FileCopyIcon />
+                  <IconButton className={classes.menuItem} aria-label='copy'>
+                    <FileCopy />
                   </IconButton>
                 )}
               </MenuItem>
               {room && (
-                <MenuItem
-                  className={classes.menuItem}
-                  onClick={() => { }}
-                >
-                  Leave
-                </MenuItem>
+                <>
+                  <a
+                    style={{ textDecoration: 'none', color: 'white' }}
+                    href={`whatsapp://send?text=${get(
+                      window.location.href.split('?'),
+                      '0',
+                      'defultRoute'
+                    )}?roomId=${room}`}
+                  >
+                    <MenuItem className={classes.menuItem}>
+                      Share to WhatsApp
+                      <IconButton
+                        className={classes.menuItem}
+                        aria-label='copy'
+                      >
+                        <WhatsApp />
+                      </IconButton>
+                    </MenuItem>
+                  </a>
+                  <MenuItem className={classes.menuItem} onClick={() => {}}>
+                    Leave
+                  </MenuItem>
+                </>
               )}
             </div>
           </Menu>
